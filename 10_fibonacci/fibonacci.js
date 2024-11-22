c72fdaebd1b447796683c3e69964ad8fe1d248cf
ludic <notetakingforeggs@gmail.com>
2024-11-22 11:31:21 +0000

    Feat: ex10 all tests passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0) return "OOPS";
+    if(num == 0) return 0;
+    value = 1;
+    temp = 0;
+    prevValue = 0;
+    for(let i = 1; i<num; i++){
+        temp = value;
+        value += prevValue;
+        prevValue = temp;
+    }
+    return value;
 };
 
+
+fibonacci(6);
 // Do not edit below this line
 module.exports = fibonacci;
