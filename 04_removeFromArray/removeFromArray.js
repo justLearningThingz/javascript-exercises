7455c1fefb14e8041b49fbfd3e5ccf6cb0a7bb7a
ludic <notetakingforeggs@gmail.com>
2024-11-22 09:33:56 +0000

    Feat: refactored - all tests passed and better time efficiency

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    newArray = [];
+    for(let i=0; i<array.length; i++){
+        if(!itemsToRemove.includes(array[i])) newArray.push(array[i]);
+    }
+    console.log(array);
+    return newArray;
 };
 
 // Do not edit below this line
