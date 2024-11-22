19fed657ee121a2fda1f0ed455537222aae87601
ludic <notetakingforeggs@gmail.com>
2024-11-22 09:56:33 +0000

    Feat all tests passing ex06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+
+// > Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+//unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+
+
+const leapYears = function(year) {
+if(year%100 === 0) return !(year%400); // returns false if divisible by 100 unless also divisible by 400 in which case returns true? i think - check modulo logic
+return !(year%4);
+
 
 };
 
