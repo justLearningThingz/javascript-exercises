627a4a748f85f501819e6a106e51382bbaf5ecf3
ludic <notetakingforeggs@gmail.com>
2024-11-22 10:45:04 +0000

    FeatL all tests passing 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  if(num1 === 0 && num2 === 0 ) return 0;
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return(num1-num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	sumTotal = 0;
+
+  for(let i=0; i<array.length; i++){
+    sumTotal += array[i];
+  }
+  
+  return(sumTotal);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  multiplyTotal = 1;
+  for(let i=0; i<array.length; i++){
+    multiplyTotal *= array[i];
+  }
+  return multiplyTotal;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return(Math.pow(num1, num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  factorialProduct = 1;
+  for(let i=1; i<=num; i++){
+    factorialProduct *= i;
+}	
+return factorialProduct;
 };
 
 // Do not edit below this line
