c29a8d779da52bacb0fc6b3750e598a82af10099
ludic <notetakingforeggs@gmail.com>
2024-11-22 12:36:01 +0000

    Feat: all exercises complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+let highestAge = 0 ;
+let oldestIndex =0;
 
+for(let i = 0; i<people.length; i++){
+    let died = people[i].yearOfDeath ?? (new Date).getFullYear(); 
+
+    let age = died - people[i].yearOfBirth;
+    if(age > highestAge){
+        highestAge = age;
+        oldestIndex = i;
+    }
+}
+return people[oldestIndex];
 };
 
 // Do not edit below this line
