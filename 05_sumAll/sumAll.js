fabbf596d7f3cd4b1b463493e0bd5ee3a2267ed3
ludic <notetakingforeggs@gmail.com>
2024-11-22 09:50:25 +0000

    Feat: passing all tests ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+if(num1 < 0 || num2 < 0) return "ERROR";
+if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+sum = 0;
+let lowerBound = num1<num2 ? num1 : num2;
+let upperBound = num1<num2 ? num2 : num1;
+
+for(let i = lowerBound; i <= upperBound; i++){
+    sum += i;
+} 
+return sum;
 };
 
 // Do not edit below this line
