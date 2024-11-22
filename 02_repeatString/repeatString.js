da1d338576bfddcfaa9e890f30feff251657140a
ludic <notetakingforeggs@gmail.com>
2024-11-22 09:03:50 +0000

    2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    returnStr = "";
+for(let i=0; i<num; i++){
+returnStr += str;
+}
+return returnStr;
 };
 
 // Do not edit below this line
