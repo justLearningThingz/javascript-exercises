7ed64e2a5feefbe2d01e003b74d72bff0ad08628
ludic <notetakingforeggs@gmail.com>
2024-11-22 11:16:43 +0000

    Feat: all tests passing ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g,'')
+                            
+console.log(cleanedString);
 
+return  cleanedString.split("").reverse().join("") === cleanedString;
 };
 
 // Do not edit below this line
